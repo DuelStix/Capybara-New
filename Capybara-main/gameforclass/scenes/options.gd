b68f02179c53ee2ee5e0236101e4466584352c66
Siricepick <167460828+Siricepick@users.noreply.github.com>
2025-05-02 09:40:19 -0700

    Add files via upload
    help

--- Capybara-main/gameforclass/scenes/options.gd
+++ Capybara-main/gameforclass/scenes/options.gd
@@ -1,13 +1,12 @@
 extends CanvasLayer
 
 
-func _on_graphics_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/graphics.tscn")
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
 
-
-func _on_volume_pressed() -> void:
+func _on_volume_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/volume.tscn")
 
 
-func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+func _on_graphics_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/graphics.tscn")
