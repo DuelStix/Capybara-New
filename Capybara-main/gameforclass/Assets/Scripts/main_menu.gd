3458507da5db424734ff2ac164ee8d5e248de591
oven-schmuvven2 <166764050+oven-schmuvven2@users.noreply.github.com>
2025-05-08 09:11:27 -0700

    Add files via upload

---
+++ Capybara-main/gameforclass/Assets/Scripts/main_menu.gd
@@ -0,0 +1,14 @@
+class_name MainMenu
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
